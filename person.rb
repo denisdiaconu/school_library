edce076c870f8171b616935af6ef48dc3d075119
denisdiaconu <denisdiaconu117@yahoo.com>
2021-10-07 10:22:20 +0200

    Fix problem with private

--- person.rb
+++ person.rb
@@ -13,19 +13,17 @@ class Person
     @corrector = Corrector.new
   end
 
-  private
-
-  def is_of_age?
-    @age >= 18
-  end
-
-  public
-
   def can_use_services?
     is_of_age? || @parent_permission ? true : false
   end
 
   def validate_name
     @name = @corrector.correct_name(@name)
+  end
+
+  private
+
+  def is_of_age?
+    @age >= 18
   end
 end
